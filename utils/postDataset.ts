e8399fe187d9f0b7c086f3f681d5c64e13197a4f
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-04-05 17:53:59 +0100

    WIP: building out signatures (#2)
    * WIP: building out signatures
    
    * chore: fix signature
    
    * chore: fix locfile

--- utils/postDataset.ts
+++ utils/postDataset.ts
@@ -1,17 +1,23 @@
 import { DatasetCore } from '@rdfjs/types';
 import { Writer } from 'n3';
+import { write } from '@jeswr/pretty-turtle';
 
 export async function postDataset(url: string, data: DatasetCore) {
   const writer = new Writer({
-    format: 'application/trig',
+    format: 'application/trig-star',
   
   });
+  // const res = [...data].map(quad => quad.graph);
+  // console.log(res);
   return fetch(url, {
     method: 'POST',
     headers: {
       // 'Content-Type': 'text/turtle',
       'Content-Type': 'application/trig',
     },
-    body: writer.quadsToString([...data]),
+    // body: writer.quadsToString([...data]),
+    body: await write([...data], {
+      format: 'text/turtle',
+    }),
   });
 }
