90db9b413a43cc586ebdd708af5e2f02f747c0a3
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-04-05 17:02:42 +0100

    chore: migrate to use @jeswr/shacl2shex

--- conversions/shacl2shex/convert.sh
+++
@@ -1,7 +0,0 @@
-for file in ./shapes/*.ttl; do
-    filename=$(basename "$file")
-    filename="${filename%.*}"
-    docker run -v "$file":/shape.ttl jeswr/shacl2shex > ./shapes/"$filename".shex
-    sed -i '1,22d;$d' ./shapes/"$filename".shex
-    sed -i 's/^.\{7\}//' ./shapes/"$filename".shex
-done
